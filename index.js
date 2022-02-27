61e244824f71f19413f0bd57e6b87ffda6b1a117
IsoTomi <tomiisoj1984@gmail.com>
2022-02-27 17:26:41 +0200

    Testing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 80
 // JSON body-parser middleware. Express 4.16+ has it's own body-parser
 // so no third party package is needed.
 app.use(express.json())
-
+/*
 // Routes
 const users = require('./routes/users')
 const items = require('./routes/items')
@@ -21,17 +21,13 @@ app.use('/items', items)
 // Cookie parser
 const cookieParser = require('cookie-parser')
 app.use(cookieParser(secrets.jwtSignKey));
-
+*/
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
-
 // POST /singin - Sing in operation.
-app.post('/signin', passport.authenticate('basic', { session: false }), (req, res) => {
+/*app.post('/signin', passport.authenticate('basic', { session: false }), (req, res) => {
   const payloadData = {
     userId: req.user.id
   }
@@ -40,7 +36,7 @@ app.post('/signin', passport.authenticate('basic', { session: false }), (req, re
   res.cookie('userId', req.user.id, {signed: true})
   res.json({ token: token })
 })
-
+*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
